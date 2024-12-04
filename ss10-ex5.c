c583eaffd181b6d9af7747b71ade1ef6d1b94edf
BMVu2403 <buiminhvu06hn@gmail.com>
2024-12-04 10:44:03 +0700

    Add files via upload

--- ss10-ex5.c
+++ ss10-ex5.c
@@ -1,52 +1,47 @@
-#include<stdio.h>
-
-void BubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
-int BinarySearch(int arr[], int n, int x) {
-    int left = 0, right = n - 1;
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        if (arr[mid] == x) return mid; 
-        if (arr[mid] < x) left = mid + 1;
-        else right = mid - 1;
-    }
-    return -1; 
-}
+#include <stdio.h>
 
 int main() {
     int n, x;
-
-    printf("Nhap so phan tu: ");
+    printf("Nhap so phan tu cua mang: ");
     scanf("%d", &n);
     int arr[n];
-    printf("Nhap cac phan tu: ");
-    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
-
-    BubbleSort(arr, n);
-
-    printf("Mang da sap xep: ");
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("Mang sau khi sap xep:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\n");
-
     printf("Nhap so can tim: ");
     scanf("%d", &x);
-    int result = BinarySearch(arr, n, x);
-
-    if (result != -1)
-        printf("Tim thay %d tai vi tri %d.\n", x, result);
-    else
-        printf("Khong tim thay %d trong mang.\n", x);
-
+    int low = 0, high = n - 1, mid, result = -1;
+    while (low <= high) {
+        mid = low + (high - low) / 2;       
+        if (arr[mid] == x) {
+            result = mid;
+            break;
+        } else if (arr[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    if (result != -1) {
+        printf("So %d duoc tim thay tai vi tri %d la .\n", x, result);
+    } else {
+        printf("Khong tim thay so %d trong mang.\n", x);
+    }
     return 0;
 }
 
